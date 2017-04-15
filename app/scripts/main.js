529f606faedf9f5710a6c8dfe1dfa90a19bcd7b8
Joseph ODriscoll <joey@jodriscoll.com>
2017-04-15 12:47:46 -0400

    initial commit to begin tracking and support localhost for browser-sync experiment

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+var entryNo = $('#to-do-list li').length;
+
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  var to_do = $('#to-do-add').val();
+  var str = [];
+  str.push('<li><input type="checkbox" id="cb');
+  entryNo++;
+  str.push(entryNo);
+  str.push('"><label for="cb');
+  str.push(entryNo);
+  str.push('">');
+  str.push(to_do);
+  str.push('</li>');
+  $('#to-do-list').append(str.join(''));
+  $('#to-do-add').val('');
+  return false;
+});
+
+console.log('Hello World');
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+/*
+  # gulp api
+    - gulp.task creats a new task
+      it requires to return either a Stream, a Promise, or an Observable
+    - gulp.src "globs" files and returns a stream of virtual file objects
+      each file can be piped through a process (jshint, uglify, less, etc.)
+    - gulp.dest saves the file back to the file system
+    - const are npm requirements while gulp is running (stored within node_modules/)
+    - the second parameter of gulp.task is always a function
+    - gulp.series is a task function that runs tasks in sequential order.
+    - gulp.parallel is task function that starts every task concurrently
+    - both task functions accept task names and other functions as parameters
+    - the can be combined infinitely...
+**/
+
+// constant requirements
+const gulp      = require('gulp');
+const uglify    = require('gulp-uglify');
+const concat    = require('gulp-concat');
+const sass      = require('gulp-sass');
+const cssmin    = require('gulp-clean-css');
+const jshint    = require('gulp-jshint');
+const del       = require('del');
+const bsync     = require('browser-sync');
+
+
+// gulp default task and standard function through ECMA6
+// gulp.task('default', () => {} );
+
+gulp.task('clean', () => {
+  // del module is the most direct way to manage files within node
+  return del('dist/**/*');
+});
+
+// javascript validator (not overly strict)
+gulp.task('lint', () => {
+  // *.js     = all files with the file type of .js
+  // **/      = all files within any directory
+  // !app/**  = ignore files/directories meeting wildcard placements
+  return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])             // first glob everything, then throw away what you don't need
+    .pipe(jshint())                                                                   // jshint alters virtual file contents
+    .pipe(jshint.reporter('default'))                                                 // report if we've wrote good/bad javascript
+    .pipe(jshint.reporter('fail'));                                                   // fail if we haven't done so
+});
+
+// run the javascript task pipeline
+gulp.task('scripts', gulp.series('lint', () => {
+  // **/*     = gulp adds everything (including folders) captured in the wildcard
+  //            during .dest()
+  //            * note: .concat() breaks this catch-all
+  return gulp.src('app/scripts/**/*.js')
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest('dist'));
+}));
+
+// run the css task pipeline
+gulp.task('styles', () => {
+  return gulp.src('app/styles/main.scss')
+    .pipe(sass())
+    .pipe(cssmin())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', (done) => {
+  bsync({
+    server: {
+      // serve files from dist/ (or if not found app/)
+      baseDir: ['dist', 'app']
+    }
+  });
+  done();
+})
+
+// when the default task is executed, parallel additional gulp tasks previously defined
+// $ gulp
+// gulp.task('default', gulp.parallel(
+//   'styles',
+//   gulp.series('lint', 'scripts', (done) => {
+//     // when lint and scripts have completed, alert the console of the completion
+//     console.log('\u2713 JavaScript Series Completed!');
+//     // nothing to return here; telling gulp that after this execution above, the whole thing is "done"
+//     done();
+//   })));
+
+gulp.task('default', gulp.series('clean',
+  gulp.parallel('styles', 'scripts'), 'server', (done) => {
+    gulp.watch('app/styles/**.scss', gulp.parallel('styles'));
+    gulp.watch('app/scripts/**.js', gulp.parallel('lint', 'scripts'));
+    gulp.watch('dist/**/*', bsync.reload);
+    done();
+  }
+));
